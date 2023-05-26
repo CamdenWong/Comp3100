94b11929c40bb14e7ab9e4860c4d2cdb75fb807d
CamdenWong <100101851+CamdenWong@users.noreply.github.com>
2023-05-26 15:08:59 +1000

    add comment

--- Comp3100_Project/MyClient2.java
+++ Comp3100_Project/MyClient2.java
@@ -7,14 +7,12 @@ public class MyClient2 {
     private static Socket s;
     private static DataOutputStream dout;
     private static BufferedReader din;
-    private static int count = 0;
     private static String lastRec = "";
     private static int jobID = 0;
     private static int jcore = 0;
     private static int jm = 0;
     private static int jd = 0;
     private static int id = 0;
-    private static Boolean finded = false;
     private static String type = "";
 
     public static void main(String[] args) throws Exception {
@@ -55,6 +53,7 @@ public class MyClient2 {
     }
 
     private static void getStage2server() throws Exception {
+        //first find the server that current availability 
         send(("GETS Avail " + jcore + " " + jm + " " + jd));
         Receive();
         getLastRec();
@@ -65,12 +64,10 @@ public class MyClient2 {
         int nRecs = Integer.parseInt(data[1]);
         send("OK");
 
+        // means current not have
         if (nRecs == 0) {
-            send("OK");
-            Receive();
-            Receive();
+            // to find sever that eventually can proive
             getStage2Capable();
-
         } else {
 
             for (int i = 0; i < nRecs; i++) {
@@ -82,12 +79,11 @@ public class MyClient2 {
             }
 
             send("OK");
-
             Receive();
 
             // find the firstServertype
             Servers server = findfirstServer();
-            type = server.Serverstype;
+            type = server.getServerType();
             id = server.getid();
             serverlist.clear();
         }
@@ -98,7 +94,6 @@ public class MyClient2 {
         send(("GETS Capable " + jcore + " " + jm + " " + jd));
         Receive();
         Receive();
-
         getLastRec();
         String str = getLastRec();
         String[] data = str.split(" ");
@@ -113,15 +108,13 @@ public class MyClient2 {
             Servers server = new Servers(datas);
             serverlist.add(server);
         }
-        // Receive();
-        send("OK");
 
+        send("OK");
         Receive();
 
-        // find the largerServertype
+        // find the Server that have the less estimated waiting time with the job
         Servers Server = LessWaitingtimeServer();
-        // Servers Server = findfirstServer();
-        type = Server.Serverstype;
+        type = Server.getServerType();
         id = Server.getid();
         serverlist.clear();
 
@@ -133,14 +126,17 @@ public class MyClient2 {
         Servers selectedServer = null;
 
         for (Servers server : serverlist) {
+            //find each server's waiting time with the job
             send(("EJWT " + server.getServerType() + " " + server.getid()));
             Receive();
             int waitingTime = Integer.parseInt(getLastRec());
 
+            //find the less one
             if (waitingTime < minWaitingTime) {
                 minWaitingTime = waitingTime;
                 minCoreCount = server.getCore();
                 selectedServer = server;
+                //If the waiting time is the same, find a less curr core server which avoid wasting core
             } else if (waitingTime == minWaitingTime) {
                 if (server.getCore() < minCoreCount) {
                     minCoreCount = server.getCore();
@@ -193,27 +189,27 @@ public class MyClient2 {
 }
 
 class Servers {
-    public String Serverstype;
-    private int Servercore;
-    private int Serverid;
+    public String ServerType;
+    private int ServerCore;
+    private int ServerId;
 
     public Servers(String DATA) {
         String[] data = DATA.split(" ");
-        this.Serverstype = data[0];
-        this.Servercore = Integer.parseInt(data[4]);
-        this.Serverid = Integer.parseInt(data[1]);
+        this.ServerType = data[0];
+        this.ServerCore = Integer.parseInt(data[4]);
+        this.ServerId = Integer.parseInt(data[1]);
     }
 
     public String getServerType() {
-        return Serverstype;
+        return ServerType;
     }
 
     public int getCore() {
-        return Servercore;
+        return ServerCore;
     }
 
     public int getid() {
-        return Serverid;
+        return ServerId;
     }
 
 }
